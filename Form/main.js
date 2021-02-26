b88979ad052cf88ed240f69466d14cd7c0e9fdb8
yassin-bouchlouch <76523296+yassin-bouchlouch@users.noreply.github.com>
2021-02-26 12:12:42 +0100

    first commit

---
+++ Form/main.js
@@ -0,0 +1,24 @@
+function om() {
+    var text=document.getElementById("Nom").value;
+    document.getElementById ("p").innerHTML+="<b>Nom:</b>&nbsp;"+text+"<br>";
+        
+            var text=document.getElementById("Prénom").value;
+            document.getElementById ("p").innerHTML+="<b>Prénome:</b>&nbsp;"+text+"<br>";
+                
+            var text=document.getElementById("Adresse").value;
+            document.getElementById ("p").innerHTML+="<b>Adresse:</b>&nbsp;"+text+"<br>";
+                
+                    var text=document.getElementById("Téléphone").value;
+                    document.getElementById ("p").innerHTML+="<b>Téléphone:</b>&nbsp;"+text+"<br>";
+                   
+                    var text=document.getElementById("Email").value;
+                    document.getElementById ("p").innerHTML+="<b>Email:</b>&nbsp;"+text+"<br>";
+                
+                                 
+    
+    
+    
+                }
+    
+            
+    
